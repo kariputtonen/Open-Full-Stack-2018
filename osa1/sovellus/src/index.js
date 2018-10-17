434a8b576d50654decce5f7ae95690a835749dd4
Kari Puttonen <kari.t.puttonen@gmail.com>
2018-10-17 12:10:03 +0300

    Oppimispäiväkirja mukaan ja tehtävä 1.1 oikeassa muodossa.

--- osa1/sovellus/src/index.js
+++ osa1/sovellus/src/index.js
@@ -4,40 +4,39 @@ import ReactDOM from 'react-dom'
 const Otsikko = (props) => {
   return (
     <div>
-      <p>{props.name} - sovelluskehitys</p>
+      <h1>{props.kurssi} - sovelluskehitys</h1>
     </div>
   )
 }
 const Sisalto = (props) => {
-  return (
-    <div>
-      <p>{props.name} {props.tehtavia}</p>
-    </div>
-  )
+//  Ei kovin kaunista, mutta en vielä osaa tehdä oikein
+return (
+      <div>
+      <p>{props.kurssinosa[0]}: {props.tehtavia[0]} tehtävää</p>
+      <p>{props.kurssinosa[1]}: {props.tehtavia[1]} tehtävää</p>
+      <p>{props.kurssinosa[2]}: {props.tehtavia[2]} tehtävää</p>
+      </div>
+    )
 }
+
 const Yhteensa = (props) => {
   return (
     <div>
-      <p>Yhteensä {props.tehtavia}</p>
+      <p>Yhteensä {props.tehtavia} tehtävää</p>
     </div>
   )
 }
 const App = () => {
   const kurssi = 'Half Stack'
-  const osa1 = 'Reactin perusteet'
-  const tehtavia1 = 10
-  const osa2 = 'Tiedonvälitys propseilla'
-  const tehtavia2 = 7
-  const osa3 = 'Komponenttien tila'
-  const tehtavia3 = 14
+  const osat = ['Reactin perusteet', 'Tiedonvälitys propseilla', 'Komponenttien tila']
+  const tehtavat = [10, 7, 14]
+
   return (
     <div>
-      <h1><Otsikko name={kurssi} /></h1>
-      <p><Sisalto name={osa1} tehtavia={tehtavia1}/></p>
-      <p><Sisalto name={osa2} tehtavia={tehtavia2}/></p>
-      <p><Sisalto name={osa3} tehtavia={tehtavia3}/></p>
-      <p><Yhteensa tehtavia={tehtavia1 + tehtavia2 + tehtavia3} /></p>
-      
+      <Otsikko kurssi={kurssi} />
+      <Sisalto kurssinosa={osat} tehtavia={tehtavat}/>
+      <Yhteensa tehtavia={tehtavat[0] + tehtavat[1] + tehtavat[2]} />
+
     </div>
   )
 }
