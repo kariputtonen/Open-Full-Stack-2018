b0d32e6bfa6e162df15b9dcc6a81d854941681d9
Kari Puttonen <kari.t.puttonen@gmail.com>
2018-10-17 22:55:08 +0300

    Sitten laajennettiin kurssiolion mallia ja lisätään piste-notaatioita

--- osa1/sovellus/src/index.js
+++ osa1/sovellus/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom'
 const Otsikko = (props) => {
   return (
     <div>
-      <h1>{props.kurssi}</h1>
+      <h1>{props.kurssi.nimi}</h1>
     </div>
   )
 }
@@ -12,11 +12,12 @@ const Sisalto = (props) => {
 //  Ei kovin kaunista, mutta en vielä osaa tehdä oikein
 // nyt kyllä tahmaa eteneminen. En ymmärrä kuinka tuo oliotaulukko tänne välitettäisiin.
 // ja ilmeisesti selvisi tyyliin: {props.osat[0].nimi}
+// Ja sitten laajennettiin kurssio-olion mallia ja lisätään piste-notaatioita
   return (
     <div>
-      <Osa kurssinosa={props.osat[0].nimi} tehtavia={props.osat[0].tehtavia} />
-      <Osa kurssinosa={props.osat[1].nimi} tehtavia={props.osat[1].tehtavia} />
-      <Osa kurssinosa={props.osat[2].nimi} tehtavia={props.osat[2].tehtavia} />
+      <Osa kurssinosa={props.kurssi.osat[0].nimi} tehtavia={props.kurssi.osat[0].tehtavia} />
+      <Osa kurssinosa={props.kurssi.osat[1].nimi} tehtavia={props.kurssi.osat[1].tehtavia} />
+      <Osa kurssinosa={props.kurssi.osat[2].nimi} tehtavia={props.kurssi.osat[2].tehtavia} />
     </div>
   )
 }
@@ -29,31 +30,33 @@ const Osa = (props) => {
 const Yhteensa = (props) => {
   return (
     <div>
-      <p>Yhteensä {props.osat[0].tehtavia + props.osat[1].tehtavia + props.osat[2].tehtavia} tehtävää</p>
+      <p>Yhteensä {props.kurssi.osat[0].tehtavia + props.kurssi.osat[1].tehtavia + props.kurssi.osat[2].tehtavia} tehtävää</p>
     </div>
   )
 }
 const App = () => {
-  const kurssi = 'Half Stack -sovelluskehitys'
-  const osat = [
-    {
-      nimi: 'Reactin perusteet',
-      tehtavia: 10
-    },
-    {
-      nimi: 'Tiedonvälitys propseilla',
-      tehtavia: 7
-    },
-    {
-      nimi: 'Komponenttien tila',
-      tehtavia: 14
-    }
-  ]
+  const kurssi = {
+    nimi: 'Half Stack -sovelluskehitys',
+    osat: [
+      {
+        nimi: 'Reactin perusteet',
+        tehtavia: 10
+      },
+      {
+        nimi: 'Tiedonvälitys propseilla',
+        tehtavia: 7
+      },
+      {
+        nimi: 'Komponenttien tila',
+        tehtavia: 14
+      }
+    ]
+  }
 return (
     <div>
       <Otsikko kurssi={kurssi} />
-      <Sisalto osat={osat} />
-      <Yhteensa osat={osat} />
+      <Sisalto kurssi={kurssi} />
+      <Yhteensa kurssi={kurssi} />
     </div>
   )
 }
